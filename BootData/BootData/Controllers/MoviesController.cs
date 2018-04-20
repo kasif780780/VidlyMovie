3a978ce9db7c812048f0301a4c8353d86344081b
Kasif Khan <kasif780780@gmail.com>
2018-04-21 04:03:43 +0530

    adding view model page

--- BootData/BootData/Controllers/MoviesController.cs
+++ BootData/BootData/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using BootData.Models;
+using BootData.Models.MovieViewModel;
 
 namespace BootData.Controllers
 {
@@ -13,7 +14,20 @@ namespace BootData.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Harry Potter" };
-            return View(movie);
+            var customer = new List<Customer>
+            {
+               new Customer {Name="Kishoew",Id=1 },
+               new Customer {Name="aman",Id=2 },
+               new Customer {Name="jeff",Id=3 },
+               new Customer {Name="mark",Id=4 }
+            };
+
+            var viewModel = new RandomViewModel
+            {
+                Movie = movie,
+                Customer = customer
+            };
+            return View(viewModel);
         }
     }
 }
--- BootData/BootData/Models/MovieViewModel/RandomViewModel.cs
+++ BootData/BootData/Models/MovieViewModel/RandomViewModel.cs
@@ -8,6 +8,6 @@ namespace BootData.Models.MovieViewModel
     public class RandomViewModel
     {
         public Movie Movie { get; set; }
-        public Customer Customer { get; set; }
+        public List<Customer> Customer { get; set; }
     }
 }
