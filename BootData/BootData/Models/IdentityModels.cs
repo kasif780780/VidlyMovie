bb23a11df08c4bfd9701fa4d5475bf2ee831965e
Kasif Khan <kasif780780@gmail.com>
2018-04-21 04:28:30 +0530

    Adding Migration

--- BootData/BootData/Models/IdentityModels.cs
+++ BootData/BootData/Models/IdentityModels.cs
@@ -20,10 +20,14 @@ namespace BootData.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Customer> Customers { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
+            
         {
         }
+       
 
         public static ApplicationDbContext Create()
         {
