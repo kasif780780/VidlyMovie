eac84536244abf357d1c2fc02423f6e6ec58325a
Kasif Khan <kasif780780@gmail.com>
2018-04-21 03:40:33 +0530

    adding viewModel

---
+++ BootData/BootData/Models/MovieViewModel/RandomViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BootData.Models.MovieViewModel
+{
+    public class RandomViewModel
+    {
+        public Movie Movie { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
