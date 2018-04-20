d1d0ab1e0e7f02567e203f6124f57a3d019fa622
Kasif Khan <kasif780780@gmail.com>
2018-04-21 02:48:10 +0530

    adding Movie Model

---
+++ BootData/BootData/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BootData.Startup))]
+namespace BootData
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
